2945e118af25f8fa9bc3198988cbdeed2f0bf076
xu.hr <xu.hr@pg.com>
2026-06-15 10:30:24 +0800

    Remove hardcoded MATRES_ALLOWED_SUBNETS from startup scripts

--- update_and_start_matres-4CV823WZKZ-W10.bat
+++ update_and_start_matres-4CV823WZKZ-W10.bat
@@ -194,7 +194,6 @@ echo ========================================
 echo.
 
 REM ========== 8) 启动看板（生产模式 Waitress）==========
-set "MATRES_ALLOWED_SUBNETS=10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,127.0.0.0/8,143.35.13.175/32"
 python -m waitress --listen=0.0.0.0:8050 dashboards.matres_app:app.server
 
 popd
